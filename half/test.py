2ca7ddac8f4428a30da46fdf591ab12c0935ab48
konglingyu2745 <kongandmarx@163.com>
2018-11-29 21:49:01 +0800

    first

--- half/test.py
+++ half/test.py
@@ -1,14 +1,8 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# Created on 2018/11/7
-import subprocess
-import requests
-from selenium import webdriver
+import time
 
-driver = webdriver.PhantomJS(executable_path=r'C:\Program Files (x86)\phantomjs-2.1.1-windows\bin\phantomjs')
-driver.get('http://zhyq.hqiic.com/cockpit/front/pages/index.html#/app/zsxx')
-tar = driver.page_source
-tar = tar
-with open('index.html', 'w+',encoding='utf-8') as  f:
-    f.writelines(tar)
-driver.quit()
+def test1():
+    print('test1')
+test1()
+
+def funcname(parameter_list):
+    pass	
---
+++ wheel/download_bing_daily.py
@@ -0,0 +1,54 @@
+#!usr/bin/env python 
+# -*-coding :utf-8-*-
+__author__ = 'Afreto'
+__date__ = '2018/4/17 8:48'
+
+import requests, re, time,os
+# import  win32api, win32gui, win32con
+from datetime import timedelta, datetime
+
+
+def download_wallpaper():
+    ''':return bool'''
+    now = time.strftime('%m_%d', time.localtime())
+    png = now + '.png'
+    if os.path.exists(png):
+        print(png + ' exited')
+    else:
+        try:
+            res = requests.get('http://area.sinaapp.com/bingImg/')
+            with open(png, 'wb')as f:
+                f.write(res.content)
+            return 1
+        except:
+            return 0
+
+
+def wallpaper_name():
+    now = time.strftime('%m_%d', time.localtime())
+    name = now + '.png'
+    if os.path.exists(name):
+        paper = os.path.abspath(name)
+        return paper
+    else:
+        return 0
+
+
+def set_wallpaper(png):
+    key = win32api.RegOpenKeyEx(win32con.HKEY_CURRENT_USER,
+                                "Control Panel\\Desktop", 0, win32con.KEY_SET_VALUE)
+    win32api.RegSetValueEx(key, "WallpaperStyle", 0, win32con.REG_SZ, "2")
+    # 2拉伸适应桌面,0桌面居中
+    # win32api.RegSetValueEx(key, "TileWallpaper", 0, win32con.REG_SZ, "0")
+    win32gui.SystemParametersInfo(win32con.SPI_SETDESKWALLPAPER, png, 1 + 2)
+
+
+if __name__ == '__main__':
+    while 1:
+        if wallpaper_name():
+            # set_wallpaper(png=wallpaper_name())
+            break
+        else:
+            download_wallpaper()
+
+    pass
