0b13f39388612f85d4a2b3d4cdecd6c025be510f
konglingyu <Neoclub2018>
2019-04-15 15:22:36 +0800

    onekey-omzsh.sh

--- wheel/onekey-omzsh/onkey-omzsh.sh
+++ wheel/onekey-omzsh/onkey-omzsh.sh
@@ -7,15 +7,10 @@ wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 # 下载 .zshrc
-#git clone https://gist.github.com/63281965e83dfbea69a8601a5da10476.git
+wget https://raw.githubusercontent.com/kong5664546498/half_a_wheel/master/wheel/onekey-omzsh/.zshrc
 
 # mv /root/.zshrc /root/.zshrc.bk
-#mv 63281965e83dfbea69a8601a5da10476/.zshrc /root/.zshrc
-
-# source /root/.zshrc
-
 chsh -s /bin/zsh
-rm -rf 63281965e83dfbea69a8601a5da10476
 zsh
 
 
