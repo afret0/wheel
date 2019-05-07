cde97d43941fb0f0cdfa719a61cb9e52f5a7f633
afreto <kongandmarx@163.com>
2019-05-07 18:41:52 +0800

    添加 .gitignore 模板

--- wheel/exts.py
+++ wheel/exts.py
@@ -6,26 +6,10 @@
 '''
 
 import os
-from config import LocConfig, DevConfig, ProConfig
+from icecream import ic
 
 
 def __res(code: int = 0, message: str = 'success', data='') -> dict:
     if data == '':
         return {'code': code, 'message': message}
     return {'code': code, 'message': message, 'data': data}
-
-
-def get_config() -> object:
-    """
-    根据环境变量 ENV 切换配置
-    :return: 配置
-    """
-    env = os.environ.get('ENV', 'loc')
-    rules = {'loc': LocConfig, 'dev': DevConfig, 'pro': ProConfig}
-    return rules.get(env)
-
-
-config = get_config()
-
-
-
