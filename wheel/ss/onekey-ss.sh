8a4d3fe092239fe76781bf52c2f48442a12f79d1
afreto <kongandmarx@163.com>
2019-09-19 10:43:01 +0800

    feat ss

--- wheel/ss/onekey-ss.sh
+++ wheel/ss/onekey-ss.sh
@@ -4,7 +4,7 @@ systemctl start docker
 systemctl enable docker 
 docker pull teddysun/shadowsocks-r
 mkdir -p  /etc/shadowsocks-r 
-wget -O /etc/shadowsocks-r/config.json https://raw.githubusercontent.com/kong5664546498/half_a_wheel/master/wheel/config.json
+wget -O /etc/shadowsocks-r/config.json https://raw.githubusercontent.com/kong5664546498/half_a_wheel/master/wheel/ss/config.json
 docker run -d -p 80:80 -p 80:80/udp --name ssr -v /etc/shadowsocks-r:/etc/shadowsocks-r teddysun/shadowsocks-r 
 # bbr
 echo y | wget --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh && chmod +x bbr.sh && ./bbr.sh -y
