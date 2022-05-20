009523efc90b8ff18bcdf120e75931c769422609
Afreto <kong5664546498@gmail.com>
2022-05-21 01:47:33 +0800

    new

--- wheel/smtp/main.py
+++ wheel/smtp/main.py
@@ -13,8 +13,8 @@ class EmailSender:
     def __init__(self) -> None:
         self.receiver = "kongandmarx@163.com"
         self.sender = "kongandmarx@163.com"
-        self.smtp_obj = smtplib.SMTP()
-        self.smtp_obj.connect("smtp.163.com", 25)
+        self.smtp_obj = smtplib.SMTP_SSL("smtp.163.com",port=994)
+        # self.smtp_obj.connect("smtp.163.com", 25)
         self.smtp_obj.login("kongandmarx@163.com", "YVLZXZWJBYAHLCAJ")
 
     def send(self, to, subject, text):
