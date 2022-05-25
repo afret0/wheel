d7096be62ace9fcf278e24b6d85ea49c32bfafe5
Afreto <kong5664546498@gmail.com>
2022-05-26 00:26:05 +0800

    new

--- half/go/main.go
+++ half/go/main.go
@@ -1,34 +1,24 @@
 package main
 
 import (
-	"log"
-	"time"
-
-	"github.com/gin-gonic/gin"
+	"fmt"
 )
 
-type Person struct {
-	Name     string    `form:"name"`
-	Address  string    `form:"address"`
-	Birthday time.Time `form:"birthday" time_format:"2006-01-02" time_utc:"1"`
+func a(){
+	fmt.Println(1)
+}
+
+func b(){
+	defer a()
+	fmt.Println(2)
+}
+
+func c()  {
+
+	fmt.Println(3)
 }
 
 func main() {
-	route := gin.Default()
-	route.GET("/testing", startPage)
-	route.Run(":8085")
-}
-
-func startPage(c *gin.Context) {
-	var person Person
-	// If `GET`, only `Form` binding engine (`query`) used.
-	// If `POST`, first checks the `content-type` for `JSON` or `XML`, then uses `Form` (`form-data`).
-	// See more at https://github.com/gin-gonic/gin/blob/master/binding/binding.go#L48
-	if c.ShouldBind(&person) == nil {
-		log.Println(person.Name)
-		log.Println(person.Address)
-		log.Println(person.Birthday)
-	}
-
-	c.String(200, "Success")
+	b()
+	c()
 }
