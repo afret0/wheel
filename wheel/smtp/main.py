f6cf8ed24c8891a8cfea55bbc73b3d947a5b3293
Afreto <kong5664546498@gmail.com>
2022-06-05 00:20:53 +0800

    1

--- wheel/smtp/main.py
+++ wheel/smtp/main.py
@@ -1,5 +1,4 @@
 import smtplib
-import time
 from email.header import Header
 from email.mime.text import MIMEText
 from flask import Flask
@@ -13,18 +12,19 @@ class EmailSender:
     def __init__(self) -> None:
         self.receiver = "kongandmarx@163.com"
         self.sender = "kongandmarx@163.com"
-        self.smtp_obj = smtplib.SMTP_SSL("smtp.163.com",port=994)
+        self.smtp_obj = smtplib.SMTP_SSL("smtp.163.com", port=994)
         # self.smtp_obj.connect("smtp.163.com", 25)
         self.smtp_obj.login("kongandmarx@163.com", "YVLZXZWJBYAHLCAJ")
 
     def send(self, to, subject, text):
-        message = MIMEText(f"{text}")
+        t = f"""
+        <h1> {text} </h1>
+        """
+        message = MIMEText(t, "html")
         message["Subject"] = Header(subject)
         message["From"] = Header(f"{self.sender}")
         message["To"] = Header(f"{to}")
         self.smtp_obj.sendmail(self.sender, self.receiver, message.as_string())
-
-
 
 
 @app.route("/", methods=["POST"])
@@ -41,5 +41,5 @@ def send_email():
 
 if __name__ == "__main__":
     # email_sender.send("18435155427@163.com", "test", "test")
-    app.run(host="0.0.0.0",port=8080, debug=False)
+    app.run(host="0.0.0.0", port=8080, debug=False)
     pass
