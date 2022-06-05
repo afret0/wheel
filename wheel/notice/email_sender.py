d50c2359ac7dda55f54dd08bb588091eb6232b81
Afreto <kong5664546498@gmail.com>
2022-06-05 14:12:52 +0800

    1

--- wheel/notice/email_sender.py
+++ wheel/notice/email_sender.py
@@ -1,11 +1,6 @@
 import smtplib
 from email.header import Header
 from email.mime.text import MIMEText
-from flask import Flask
-from flask import request
-from flask import jsonify
-
-app = Flask(__name__)
 
 
 class EmailSender:
@@ -25,21 +20,3 @@ class EmailSender:
         message["From"] = Header(f"{self.sender}")
         message["To"] = Header(f"{to}")
         self.smtp_obj.sendmail(self.sender, self.receiver, message.as_string())
-
-
-@app.route("/", methods=["POST"])
-def send_email():
-    data = request.get_json()
-    print(data)
-    to = data["to"]
-    subject = data["subject"]
-    text = data["text"]
-    email_sender = EmailSender()
-    email_sender.send(to, subject, text)
-    return jsonify({"code": 1, "message": "succeed"})
-
-
-if __name__ == "__main__":
-    # email_sender.send("18435155427@163.com", "test", "test")
-    app.run(host="0.0.0.0", port=8080, debug=False)
-    pass
