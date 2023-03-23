fefaa579d4891756108affc46b822620563f7653
Afreto <kong5664546498@gmail.com>
2023-03-23 19:09:25 +0800

    doc

--- half/go/main.go
+++ half/go/main.go
@@ -2,23 +2,34 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
-func a(){
+func a() {
 	fmt.Println(1)
 }
 
-func b(){
+func b() {
 	defer a()
 	fmt.Println(2)
 }
 
-func c()  {
+func c() {
 
 	fmt.Println(3)
 }
 
 func main() {
-	b()
-	c()
+	m := make(map[string]string, 0)
+	m["1"] = "a"
+	m["2"] = "b"
+	m["3"] = "c"
+	l := make([]string, 0)
+	for k, v := range m {
+		s := fmt.Sprintf("%s=%s", k, v)
+		fmt.Println(s)
+		l = append(l, s)
+	}
+	s := strings.Join(l,"&")
+	fmt.Println(l,s)
 }
