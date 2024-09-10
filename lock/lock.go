db84f4c2539491ccdcf8bff44887e6f74f7092c5
test <kong5664546498@gmail.com>
2024-09-10 19:10:12 +0800

    lock

---
+++ lock/lock.go
@@ -0,0 +1,72 @@
+package lock
+
+import (
+	"context"
+	"errors"
+	"github.com/redis/go-redis/v9"
+	"time"
+
+	"github.com/bsm/redislock"
+)
+
+type Locker struct {
+	Locker *redislock.Client
+}
+
+var locker *Locker
+
+func GetLocker(rdb redis.UniversalClient) *Locker {
+	if locker != nil {
+		return locker
+	}
+	locker = new(Locker)
+	locker.Locker = redislock.New(rdb)
+
+	return locker
+}
+
+func (l *Locker) Obtain(ctx context.Context, key string, ttl int, opt ...*redislock.Options) (*redislock.Lock, error) {
+	if key == "" {
+		return nil, errors.New("key is empty")
+	}
+
+	t := time.Second * time.Duration(5)
+	if ttl != 0 {
+		t = time.Duration(ttl) * time.Second
+	}
+
+	optN := new(redislock.Options)
+	if len(opt) != 0 {
+		optN = opt[0]
+	}
+
+	return l.Locker.Obtain(ctx, key, t, optN)
+}
+
+func (l *Locker) ObtainWaitRetry(ctx context.Context, key string, ttl int, retryCount int, retryDelay int) (*redislock.Lock, error) {
+	timeoutCtx, cancel := context.WithTimeout(ctx, time.Duration(retryDelay*retryCount)*time.Second)
+	defer cancel()
+
+	for {
+		select {
+		case <-timeoutCtx.Done():
+			// If the context is done (because of the timeout), return an error
+			return nil, errors.New("timeout while trying to obtain the lock")
+		default:
+			// Try to obtain the lock
+			lock, err := l.Locker.Obtain(ctx, key, time.Duration(ttl)*time.Second, nil)
+			if err != nil {
+				if errors.Is(err, redislock.ErrNotObtained) {
+					// If the lock is not obtained, wait for a while before trying again
+					time.Sleep(time.Duration(retryDelay) * time.Second)
+					continue
+				}
+				// If there is another error, return it
+				return nil, err
+			}
+
+			// If the lock is obtained, return it
+			return lock, nil
+		}
+	}
+}
