b68cb3fe6fa9d6e85c5ff2ddcfcdafcfa43d0f89
test <kong5664546498@gmail.com>
2024-11-19 19:53:12 +0800

    fix

--- frame/tool.go
+++ frame/tool.go
@@ -7,10 +7,24 @@ import (
 )
 
 func Request(ctx context.Context) *http.Request {
-	return ctx.Value(gin.ContextRequestKey).(*http.Request)
+	req := ctx.Value(gin.ContextRequestKey)
+	if req == nil {
+		return nil
+	}
+
+	req1, ok := req.(*http.Request)
+	if !ok {
+		return nil
+	}
+
+	return req1
 }
 
 func Header(ctx context.Context) http.Header {
 	req := Request(ctx)
+	if req == nil {
+		return nil
+	}
+
 	return req.Header
 }
