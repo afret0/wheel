a5c27640986f8c4c01f32a717ae29318e6d03aab
test <kong5664546498@gmail.com>
2024-11-29 16:27:00 +0800

    f lastmouth

--- tool/time_test.go
+++ tool/time_test.go
@@ -6,9 +6,11 @@ import (
 )
 
 func Test_time(t *testing.T) {
+	fmt.Printf("LastWeek: %s\n", LastWeek())
 	fmt.Printf("Week: %s\n", Week())
 	fmt.Printf("WeekDay: %s\n", WeekDay())
 	fmt.Printf("Year: %s\n", Year())
+	fmt.Printf("LastMonth: %s\n", LastMonth())
 	fmt.Printf("Month: %s\n", Month())
 	fmt.Printf("Day: %s\n", Day())
 	fmt.Printf("Hour: %s\n", Hour())
