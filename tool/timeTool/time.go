35d7606fce7ba5759083497e60897f502783acdf
test <kong5664546498@gmail.com>
2024-12-06 16:05:51 +0800

    feat

--- tool/timeTool/time.go
+++ tool/timeTool/time.go
@@ -8,14 +8,14 @@ import (
 
 func Week() string {
 	year, week := time.Now().ISOWeek()
-	return fmt.Sprintf("%d%d", year, week)
+	return fmt.Sprintf("%d%02d", year, week)
 }
 
 func LastWeek() string {
 	// 获取7天前的时间
 	lastWeek := time.Now().AddDate(0, 0, -7)
 	year, week := lastWeek.ISOWeek()
-	return fmt.Sprintf("%d%d", year, week)
+	return fmt.Sprintf("%d%02d", year, week)
 }
 
 func WeekDay() time.Weekday {
@@ -48,24 +48,25 @@ func LastMonth() string {
 
 	return fmt.Sprintf("%d%02d", year, month)
 }
+
 func Month() string {
-	return fmt.Sprintf("%s%d", Year(), time.Now().Month())
+	return fmt.Sprintf("%s%02d", Year(), time.Now().Month())
 }
 
 func Day() string {
-	return fmt.Sprintf("%s%d", Month(), time.Now().Day())
+	return fmt.Sprintf("%s%02d", Month(), time.Now().Day())
 }
 
 func Hour() string {
-	return fmt.Sprintf("%s%d", Day(), time.Now().Hour())
+	return fmt.Sprintf("%s%02d", Day(), time.Now().Hour())
 }
 
 func Minute() string {
-	return fmt.Sprintf("%s%d", Hour(), time.Now().Minute())
+	return fmt.Sprintf("%s%02d", Hour(), time.Now().Minute())
 }
 
 func Second() string {
-	return fmt.Sprintf("%s%d", Minute(), time.Now().Second())
+	return fmt.Sprintf("%s%02d", Minute(), time.Now().Second())
 }
 
 func MidnightTody() time.Time {
