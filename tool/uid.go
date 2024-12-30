791523a1091a57599a06a1d50215b40d34d10a0e
test <kong5664546498@gmail.com>
2024-12-30 14:21:26 +0800

    f

--- tool/uid.go
+++ tool/uid.go
@@ -1,17 +1,24 @@
 package tool
 
-import "context"
+import (
+	"context"
+	"github.com/afret0/wheel/frame"
+)
 
 func Uid(ctx context.Context) string {
-	//uid := ctx.Value("_uid")
-	//return uid.(string)
-	uid, ok := ctx.Value("_uid").(string)
-	if !ok {
-		panic("uid not found in context")
-	}
+	header := frame.Header(ctx)
+	uid := header.Get("_uid")
 	if uid == "" {
-		panic("uid is empty")
+		panic("uid no exist")
 	}
+
+	//uid, ok := ctx.Value("_uid").(string)
+	//if !ok {
+	//	panic("uid not found in context")
+	//}
+	//if uid == "" {
+	//	panic("uid is empty")
+	//}
 
 	return uid
 }
