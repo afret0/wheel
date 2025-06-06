b04568ac556b6de5eea598f87b98762f68e1e4e2
afreto <kong5664546498@gmail.com>
2025-06-06 18:53:13 +0800

    handle panic

--- tool/recoverTool/recover.go
+++ tool/recoverTool/recover.go
@@ -78,24 +78,38 @@ func (r *RecoverTool) HandleRecover(ro any, stack string) {
 	//fmt.Println(ro)
 	//fmt.Println(stack)
 
-	e, ok := ro.(error)
-	if !ok {
-		r.lg.Errorf("recover tool ro.(error) failed, ro: %v", ro)
-		return
+	var errMsg string
+
+	switch v := ro.(type) {
+	case string:
+		errMsg = v
+	case error:
+		errMsg = v.Error()
+	case fmt.Stringer: // 实现了String()方法的类型
+		errMsg = v.String()
+	default:
+		// 对于其他类型，使用fmt.Sprintf转换为字符串
+		errMsg = fmt.Sprintf("%v", v)
 	}
 
-	count := r.limit.Count(e.Error())
+	//s, ok := ro.(string)
+	//if !ok {
+	//	r.lg.Errorf("recover tool ro.(error) failed, ro: %v", ro)
+	//	return
+	//}
+
+	count := r.limit.Count(errMsg)
 	//fmt.Printf("count: %d\n", count)
 	if count > 0 {
-		r.lg.Infof("recover tool already handled this error, count: %d, error: %s", count, e.Error())
+		r.lg.Infof("recover tool already handled this error, count: %d, error: %s", count, errMsg)
 		return
 	}
 
-	r.limit.Incr(e.Error())
+	r.limit.Incr(errMsg)
 
-	htmlContent := formatHtml(r.service, e.Error(), stack)
+	htmlContent := formatHtml(r.service, errMsg, stack)
 
-	err := r.emailSvc.Send(r.emailReceiver, fmt.Sprintf(" [ %s ]  %s [ %s ] ", r.service, e.Error(), r.env), htmlContent)
+	err := r.emailSvc.Send(r.emailReceiver, fmt.Sprintf(" [ %s ]  %s [ %s ] ", r.service, errMsg, r.env), htmlContent)
 	if err != nil {
 		r.lg.Errorf("err: %s", err)
 		return
