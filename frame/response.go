1c7c7937e5b1fe2e6b2661dd75c655f21b66b63a
afreto <kong5664546498@gmail.com>
2025-07-14 19:05:22 +0800

    page

--- frame/response.go
+++ frame/response.go
@@ -4,3 +4,11 @@ type BaseResponse struct {
 	Code    int    `json:"code"`
 	Message string `json:"message"`
 }
+
+type PageTag struct {
+	PageTag        string `json:"pageTag"`
+	ForwardPageTag string `json:"forwardPageTag"`
+	IsForward      bool   `json:"isForward"`
+	IsLastPage     bool   `json:"isLastPage"`
+	Count          int64  `json:"count"`
+}
