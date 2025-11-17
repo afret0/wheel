4f90da5541f318821f2db2762e1cd3897c68ad66
afreto <kong5664546498@gmail.com>
2025-11-17 18:02:48 +0800

    f

--- limiter/limiter.go
+++ limiter/limiter.go
@@ -1,21 +1,24 @@
 package limiter
 
 import (
-	"github.com/redis/go-redis/v9"
 	"time"
+
+	"github.com/redis/go-redis/v9"
 
 	"github.com/go-redis/redis_rate/v10"
 )
 
+// deprecated
 func NewLimiter(rdb redis.UniversalClient) *redis_rate.Limiter {
 	limiter := redis_rate.NewLimiter(rdb)
 	return limiter
 }
 
+// deprecated
 func PerDuration(rate int, duration time.Duration) redis_rate.Limit {
 	return redis_rate.Limit{
 		Rate:   rate,
 		Burst:  rate,
-		Period: duration / time.Duration(rate),
+		Period: duration,
 	}
 }
